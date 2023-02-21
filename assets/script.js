ffd0e7069a270be87e2e54e76ed4149daa7835b7
Jamie Nadel <jamienadel4@gmail.com>
2023-02-21 15:11:56 -0500

    added local storage functions and highscore input

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,19 @@
 // variables
 var startEl = document.querySelector('#startButton');
 var timeEl = document.querySelector('.time');
-var questionEl = document.getElementById('question');
-var questionArea = document.getElementById('questionarea');
+var questionEl = document.querySelector('#question');
+var questionArea = document.querySelector('#questionarea');
+var questionView = document.querySelector('#questionview');
+var homeScreenSection = document.querySelector('#homescreen');
+var highScoreSection = document.querySelector('#highscoreinput');
 var secondsLeft = 20;
 var currentQuestion;
 var index = 1;
+var score = 0;
+var rightOrWrong = '';
+var userScores = [];
+var submitBtnEl = document.createElement('button');
 
-// timer function
-startEl.addEventListener('click', function(event) {
-  setTime();
-  questionEl.textContent = question1.question;
-  choiceA.textContent = question1.answerChoices[0];
-  choiceB.textContent = question1.answerChoices[1];
-  choiceC.textContent = question1.answerChoices[2];
-  choiceD.textContent = question1.answerChoices[3];
-});
-
-// answer event listener
-questionArea.addEventListener('click', function(event) {
-  navigateQuestions(event.target.id);
-});
-
-timeEl.textContent = 'Time: ' + secondsLeft ;
-
-function setTime() {
-  // Sets interval in variable
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = 'Time: ' + secondsLeft ;
-
-    if(secondsLeft === 0) {
-      // Stops execution of action at set interval
-      clearInterval(timerInterval);
-      secondsLeft = 3;
-    }
-
-  }, 1000);
-}
 
 // question content
 var question1 = {
@@ -54,32 +30,65 @@ var question2 = {
 // array of questions
 var questions = [question1, question2];
 
+// timer function
+startEl.addEventListener('click', function() {
+  setTime();
+  questionEl.textContent = question1.question;
+  choiceA.textContent = question1.answerChoices[0];
+  choiceB.textContent = question1.answerChoices[1];
+  choiceC.textContent = question1.answerChoices[2];
+  choiceD.textContent = question1.answerChoices[3];
+  homeScreenSection.remove();
+});
 
-function navigateQuestions(id) {
-  console.log('index', index);
-  
-  console.log('id', id);
+// answer event listener
+questionArea.addEventListener('click', function(event) {
+  navigateQuestions(event.target.id);
+  isItCorrect(event.target.id);
+});
 
-  // if (!questions[index + 1]) {
-  //   return;
-  //   // store timeremain as highscore val n spit to submit initials n high score page
-  // }
+timeEl.textContent = 'Time: ' + secondsLeft ;
+
+function setTime() {
+
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = 'Time: ' + secondsLeft ;
+    console.log('index', index);
+
+    if(secondsLeft <= 0 || index === -1) {
+      // Stops execution of action at set interval
+      questionView.remove();
+      clearInterval(timerInterval);
+      displayHighScores();
+    }
+
+  }, 1000);
+}
+
+function isItCorrect(id) {
+  if (id === currentRightAnswer) {
+    rightOrWrong = 'right';
+    score++;
+  } else {
+    secondsLeft = secondsLeft - 5;
+    rightOrWrong = 'wrong';
+  }
+  console.log(rightOrWrong);
+}
+
+function navigateQuestions() {
+
   currentRightAnswer = questions[index - 1].isCorrect;
 
   if (!questions[index]) {
-    console.log('done');
+    index = -1;
     return;
   }
-  if (id === currentRightAnswer) {
-    console.log('you got one right');
-  } else {
-    secondsLeft = secondsLeft - 10;
-    console.log('you better have gotten it wrong');
-  }
-
   currentQuestion = questions[index].question;
-  currentAnswers = questions[index].answerChoices; // array
-  console.log('currentRightAnswer', currentRightAnswer);
+  currentAnswers = questions[index].answerChoices;
+
   // add textcontent
   questionEl.textContent = currentQuestion;
   choiceA.textContent = currentAnswers[0];
@@ -88,6 +97,43 @@ function navigateQuestions(id) {
   choiceD.textContent = currentAnswers[3];
 
   index++;
-
 }
- 
+
+function displayHighScores() {
+  highScoreSection.textContent = 'Your High Score: ' + secondsLeft;
+  var pEl = document.createElement('p');
+  var inputEl = document.createElement('input');
+
+  pEl.textContent = 'Please enter your initials:';
+  inputEl.setAttribute('id', 'initials');
+  submitBtnEl.textContent = 'Submit';
+
+  highScoreSection.appendChild(pEl);
+  highScoreSection.appendChild(inputEl);
+  highScoreSection.appendChild(submitBtnEl);
+
+} 
+
+submitBtnEl.addEventListener('click', function() {
+  var initials = document.getElementById('initials').value;
+
+  var userScore = {
+    name: initials.trim(),
+    score: score,
+    time: secondsLeft,
+  };
+  console.log('userScore', userScore);
+
+  var storedScores = JSON.parse(localStorage.getItem("userScores"));
+
+  if (storedScores !== null) {
+    userScores = storedScores;
+  }
+
+  userScores.push(userScore);
+  localStorage.setItem("userScores", JSON.stringify(userScores));
+
+});
+
+
+
